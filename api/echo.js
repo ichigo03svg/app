9aaa308a064e7c9f9153efa4752c9def5c931f24
ichigo03svg <ichigoorihime0@gmail.com>
2025-08-25 19:22:50 -0300

    Create echo.js

---
+++ api/echo.js
@@ -0,0 +1,9 @@
+export default async function handler(req,res){
+  const mask = s => s ? s.slice(0,4)+"…"+s.slice(-4) : ""
+  res.setHeader("Content-Type","application/json")
+  res.end(JSON.stringify({
+    redirect_uri: process.env.REDIRECT_URI||null,
+    client_id_preview: mask(process.env.CLIENT_ID||""),
+    env: process.env.VERCEL_ENV||""
+  }))
+}
